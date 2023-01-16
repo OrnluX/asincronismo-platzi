5a2a4fc2e6cf16897629c0e31b7b5d1ef4b553cb
OrnluX <ivantarquini91@gmail.com>
2023-01-16 07:35:14 -0300

    Añadimos varios features como sass. Un contenedor donde se renderizan los productos, y optimizamos el código fuente, quitando callbacks y usando mejores prácticas

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -2,146 +2,81 @@ const API = 'https://api.escuelajs.co/api/v1';
 const app = document.getElementById('app');
 
 function fetchData(urlApi){
-    console.log('Rendering...')
-    return fetch(urlApi).then(response => response.json());
-    
+    console.log('Fetching Data...')
+    return fetch(urlApi).then(response => response.json())
+    .then(console.log('Fetching Done'))
+    .catch(error => console.error(`Error fetching data! ${error}`));
 };
 
 
-const setAtr = (element, attributes) => {
-    for (let i = 0; i < attributes.length; i++){
-        element.setAttribute(`${attributes[i]}`, 'products');
-    }
+const setAtr = (element, {id, className}) => {
+    
+    element.setAttribute('id',id );
+    element.setAttribute('class', className);
 }
 
-function createContainer (cb) {
+const createContainer = () => {
     const section = document.createElement('section');
-    const sectionTitle = document.createElement('h1');
-    section.appendChild(sectionTitle);
-    sectionTitle.innerText='Productos';
+    const sectionTitle = document.createElement('h2');
+    const sectionContent = document.createElement('div');
+    const sectionBtn = document.createElement('button');
     
-    const atrs = ['class', 'id'];
-    cb(section, atrs);
+    sectionTitle.innerText='Productos';
+    sectionBtn.innerText='Cargar Más';
+    
+    section.appendChild(sectionTitle);
+    section.appendChild(sectionContent);
+    section.appendChild(sectionBtn);
+    
+    setAtr(section, {id: 'products', className:'products'} );
+    setAtr(sectionContent, {id: 'product-container', className: 'section-container'});
+    setAtr(sectionBtn, {id: 'products-btn', className: 'section-btn'});
+
     app.appendChild(section);
 }
 
-createContainer(setAtr);
+createContainer();
 
 
 let products = []
+let pagination = 0;
 
 const getProductData = async () => {
-    console.log('Fetching data...')
-    const response = await fetchData(`${API}/products?offset=0&limit=12`);
-    products.push(response)
-
+    
+    const response = await fetchData(`${API}/products?offset=${pagination}&limit=12`);
+    products = response;
+    console.log(products)
     return response
 }
 
-
 const setProducts = async ()=>{
-    products = await getProductData()
-    .catch(e=>console.log('Has fallado miserablemente!'));
-    //Creamos las tarjetas del producto una vez tenemos la data disponible
-    products.forEach( product => createCard(product));
+    products = await getProductData();
+    const container = document.getElementById("product-container");
+    container.append(
+      ...products.map((product) => createCard(product))
+    );
     console.log('Done!')
 }
 
 setProducts();
 
-const createCard = (product)=>{
-    const container = document.getElementById('products')
-    const card =  document.createElement('article');
-    card.setAttribute('class', 'card')
-    container.appendChild(card);
-    
+const createCard = (product) => {
+    const card = document.createElement("article");
+    card.setAttribute("class", "card");
     card.innerHTML = `
         <img loading="lazy" src="${product.images[0]}">
         <h3>${product.title}</h3>
         <span>$${product.price}</span>
         <p>${product.description}</p>
-
-    `
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const createCards = (title, price, img) =>{
-//     const card = document.createElement('article');
-//     card.setAttribute('class', 'card');
-//     app.appendChild(card);
-//     card.innerHTML = `
-//         <h1>${title}</h1>
-//         <span>$${price}</span>
-//         <img loading = "lazy" src="${img[0]}">
-//     `;
-// }
-
-
-
-// const getProductData = async () => {
-//     console.log('Fetching data...')
-//     const response = await fetchData(`${API}/products`)
-//     .then(products => {
-//         return (fetchData(`${API}/products/${products[0].id}`))
-//     })
+    `;
     
-//     .catch(e=>console.log('Has fallado!'))
-    
-//     return response;
-// }
+    return card;
+};
 
-// const mostrarData = async (callback)=>{
-//     const data = await getProductData();
-//     const cardTitle = data.title;
-//     const cardPrice = data.price;
-//     const cardImg = data.images;
-//     callback(cardTitle, cardPrice, cardImg);
-
-//     console.log(data)
-// }
-
-// mostrarData(createCards);
-
+//Pagination
+const paginationBtn = document.getElementById('products-btn');
+paginationBtn.addEventListener('click', ()=>{
+    pagination+=12;
+    console.log(pagination)
+    setProducts()
+});
